370a9095025e7e610b86a3dbd81b84d83c062390
Toni Njirić <toni.njiric@ez-booker.com>
2024-04-22 22:18:15 +0200

    Update ReservationModificationMail.php

--- app/Mail/Guest/ReservationModificationMail.php
+++ app/Mail/Guest/ReservationModificationMail.php
@@ -17,6 +17,15 @@ class ReservationModificationMail extends Mailable
         $this->reservation = Reservation::findOrFail($reservation_id);
         \App::setLocale($locale);
 
+        $display_id = $reservation_id;
+
+        if($this->reservation->is_main != 1){
+            $main_booking = Reservation::where('round_trip_id',$reservation_id)->get()->first();
+            if(!empty($main_booking->status) && $main_booking->status == 'confirmed'){
+                $display_id = $main_booking->id;
+            }
+        }
+
         switch ($this->reservation->confirmation_language){
             case 'en':
                 $booking_confirmation = 'Confirmation of your transfer modification';
@@ -37,10 +46,10 @@ class ReservationModificationMail extends Mailable
         $this->subject(__('mail.guest.modification_mail.subject'));
 
         $pdf = PDF::loadView('attachments.voucher', ['reservation'=>$this->reservation])->setPaper('A4', 'portrait');
-        $this->attachData($pdf->output(),"Voucher_{$reservation_id}.pdf");
+        $this->attachData($pdf->output(),"Voucher_{$display_id}.pdf");
 
         $pdf = PDF::loadView('attachments.booking_confirmation', ['reservation'=>$this->reservation])->setPaper('A4', 'portrait');
-        $this->attachData($pdf->output(),"{$booking_confirmation}_{$reservation_id}.pdf");
+        $this->attachData($pdf->output(),"{$booking_confirmation}_{$display_id}.pdf");
 
         if($this->reservation->hasCancellationFee()){
 
@@ -60,7 +69,7 @@ class ReservationModificationMail extends Mailable
 
 
             $pdf_cf = PDF::loadView('attachments.booking_cancellation_fee',['reservation'=>$this->reservation])->setPaper('A4', 'portrait');
-            $this->attachData($pdf_cf->output(),"{$booking_cancellation_fee}_{$reservation_id}.pdf");
+            $this->attachData($pdf_cf->output(),"{$booking_cancellation_fee}_{$display_id}.pdf");
         }
 
     }
