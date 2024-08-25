a965611f4011c7dba76a39104c895c2a065d0039
Toni Njirić <toni.njiric@ez-booker.com>
2024-08-25 17:15:37 +0200

    Update GetMailHeaderAddressAndName.php

--- app/Actions/Mail/GetMailHeaderAddressAndName.php
+++ app/Actions/Mail/GetMailHeaderAddressAndName.php
@@ -13,32 +13,7 @@ class GetMailHeaderAddressAndName
     {
 
         switch ($reservation->destination->owner_id){
-            case 1: $header = "Valamar Riviera d.d<br/>
-
-                    Stancija Kaligari 1<br/>
-                    Poreč, Republika Hrvatska<br/>
-                   <br/>
-                   OIB: 36201212847
-                   <br/>
-                   IBAN: HR4123600001101319202<br/>";
-
-                  switch(app()->getLocale()){
-                        case 'it':
-                            $header .= "<i>Normativa sulla Privacy: https://www.valamar.com/it/normativa-sulla-privacy</i>";
-                            break;
-                      case 'en':
-                          $header .= "<i>Privacy policy: https://valamar-riviera.com/en/privacy-policy/</i>";
-                          break;
-                      case 'hr':
-                          $header .= "<i>Politika Privatnosti: https://www.valamar.com/hr/izjava-o-privatnosti</i>";
-                          break;
-                      case 'de':
-                          $header .= "<i>Datenschutzrichtlinie: https://www.valamar.com/de/datenschutz</i>";
-                          break;
-                  }
-
-                break;
-            case 2:
+            case 1:
                 $header = "Valamar Riviera d.d<br/>
 
                     Stancija Kaligari 1<br/>
@@ -53,7 +28,38 @@ class GetMailHeaderAddressAndName
                         $header .= "<i>Normativa sulla Privacy: https://www.valamar.com/it/normativa-sulla-privacy</i>";
                         break;
                     case 'en':
-                        $header .= "<i>Privacy policy: https://valamar-riviera.com/en/privacy-policy/</i>";
+                        $header .= "<i>Privacy policy: https://valamar.com/en/privacy-policy/</i>";
+                        break;
+                    case 'hr':
+                        $header .= "<i>Politika Privatnosti: https://www.valamar.com/hr/izjava-o-privatnosti</i>";
+                        break;
+                    case 'de':
+                        $header .= "<i>Datenschutzrichtlinie: https://www.valamar.com/de/datenschutz</i>";
+                        break;
+                }
+
+                break;
+            case 2:
+            case 3:
+                $header = "Valamar Riviera d.d<br/>
+                    Stancija Kaligari 1<br/>
+                    Poreč, Republika Hrvatska<br/>
+                   <br/>
+                   OIB: 36201212847
+                   <br/>
+                   <i>On behalf of and for the account</i><br/>
+                   Imperial Riviera<br/>
+                   Jurja Barakovića 2<br/>
+                   Rab, Republika Hrvatska<br/>
+                   OIB: 90896496260<br/>
+                   IBAN: HR4123600001101319202<br/>";
+
+                switch(app()->getLocale()){
+                    case 'it':
+                        $header .= "<i>Normativa sulla Privacy: https://www.valamar.com/it/normativa-sulla-privacy</i>";
+                        break;
+                    case 'en':
+                        $header .= "<i>Privacy policy: https://valamar.com/en/privacy-policy/</i>";
                         break;
                     case 'hr':
                         $header .= "<i>Politika Privatnosti: https://www.valamar.com/hr/izjava-o-privatnosti</i>";
